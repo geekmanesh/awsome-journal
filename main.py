628b8926c698d35569814858ddc59dd892a0dd5e
dimah <dimah.code@gmail.com>
2026-04-19 09:42:54 +0330

    feat: add endpoint to return all todos

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+from typing import Annotated
 
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 import models
-from database import engine
+from models import Todos
+from database import engine, session_local
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    sqlite_db = session_local()
+
+    try:
+        yield sqlite_db
+    finally:
+        sqlite_db.close()
+
+
+sqlite_db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/")
+async def read_all_todos(db: sqlite_db_dependency):
+    return db.query(Todos).all()
