7bc5339f138f38cf38b5564510a9346bd9e2f9a3
dimah <dimah.code@gmail.com>
2026-04-19 09:55:45 +0330

    feat: add endpoint for getting a todo with all validations

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 from sqlalchemy.orm import Session
+from starlette import status
+
 import models
 from models import Todos
 from database import engine, session_local
@@ -23,6 +25,15 @@ def get_db():
 sqlite_db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 async def read_all_todos(db: sqlite_db_dependency):
     return db.query(Todos).all()
+
+
+@app.get("/todos/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: sqlite_db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    else:
+        raise HTTPException(status_code=404, detail="No todo found with this id!")
