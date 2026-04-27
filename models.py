6a957f044157d92841dd560b3dce0719b4acbe33
dimah <dimah.code@gmail.com>
2026-04-27 09:13:01 +0330

    feat(auth): add user model and table with a test endpoint: POST

--- models.py
+++ models.py
@@ -1,8 +1,29 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from collections import Counter
+
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from pydantic import BaseModel, Field
 
 from database import Base
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
 
 class Todos(Base):
     __tablename__ = "todos"
@@ -12,10 +33,11 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
-
+    owner_id = Column(Integer, ForeignKey("users.id"))
 
 class TodoRequest(BaseModel):
     title: str = Field(min_length=3)
     description: str = Field(min_length=3, max_length=1000)
     priority: int = Field(gt=0, lt=6)
     complete: bool
+
--- routers/auth.py
+++ routers/auth.py
@@ -1,9 +1,21 @@
+from calendar import firstweekday
+
 from fastapi import FastAPI, APIRouter
+
+from models import CreateUserRequest, User
 
 router = APIRouter()
 
 
 
-@router.get("/auth")
-async def get_user():
-    return {"user": "authenticated"}
+@router.post("/auth")
+async def create_user(create_user_request: CreateUserRequest):
+    create_user_request = User(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=create_user_request.password
+    )
+
